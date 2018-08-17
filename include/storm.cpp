bab6fe06c36b727a3618d5030f90103d0aae4863
brinkqiang <brink.qiang@gmail.com>
2018-08-17 13:52:30 +0800

    add

--- include/storm.cpp
+++ include/storm.cpp
@@ -3,6 +3,7 @@
 #include <memory>
 #include <stdarg.h>
 #include <stdlib.h>
+#include <assert.h>
 
 namespace ORM{
 
@@ -56,14 +57,73 @@ int raw_query(const std::string format, ...){
 		get_db(), query.get() );
 }
 
-std::string escape(const std::string &from){
-	static MYSQL *mysql = mysql_init( NULL );
+inline int32_t EscapeMySQLString(const char* from, int32_t from_length, char* to, int32_t to_length)
+{
+    assert(from && to);
+
+    if (from_length * 2 + 1 < to_length)
+    {
+        return -1;
+    }
+
+    int32_t result_length = 0;
+
+    for (int32_t index = 0; index < from_length; ++index)
+    {
+        char c = from[index];
+
+        switch (c)
+        {
+        case '\0':
+            to[result_length++] = '\\';
+            to[result_length++] = '0';
+            break;
+
+        case '\n':
+            to[result_length++] = '\\';
+            to[result_length++] = 'n';
+            break;
+
+        case '\r':
+            to[result_length++] = '\\';
+            to[result_length++] = 'r';
+            break;
+
+        case 0x1a:
+            to[result_length++] = '\\';
+            to[result_length++] = 'Z';
+            break;
+
+        case '\'':
+            to[result_length++] = '\\';
+            to[result_length++] = '\'';
+            break;
+
+        case '"':
+            to[result_length++] = '\\';
+            to[result_length++] = '\"';
+            break;
+
+        case '\\':
+            to[result_length++] = '\\';
+            to[result_length++] = '\\';
+            break;
+
+        default:
+            to[result_length++] = c;
+        }
+    }
+
+    return result_length;
+}
+
+std::string escape(const std::string& from)
+{
     std::string to;
     to.resize(from.size() * 2 + 1);
-	mysql_real_escape_string(
-		mysql, &*to.begin(), from.c_str(), (unsigned long)from.length() );
-
-	return to;
+    int32_t size = EscapeMySQLString(from.c_str(), from.size(), &*to.begin(), to.size());
+    to.resize(size);
+    return to;
 }
 
 }
